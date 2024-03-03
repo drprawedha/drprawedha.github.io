2bb7357f51565f8d5a335d8a04bdbc5c99c8d2c9
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-03 16:11:49 +0700

    Boolean Test

---
+++ js-exmp/boolean.js
@@ -0,0 +1,15 @@
+// Boolean itu true / false 
+
+// var tinggi = true;
+// console.log(tinggi);
+
+// studi kasus
+// jika tinggi kurang dari 165 maka pendek, selain itu maka tinggi
+
+var cm = 166
+if (cm>165) 
+    tinggi = true
+ else 
+    tinggi = false
+
+    console.log(tinggi);

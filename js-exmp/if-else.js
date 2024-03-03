afdcfc882b90e19d6bece614a8d29d5cb128d47a
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-04 00:09:06 +0700

    if else function

---
+++ js-exmp/if-else.js
@@ -0,0 +1,12 @@
+function jalansatu() {
+    var nilai = 1;
+    if (nilai === 3) {
+        console.log("tiga")
+    } else if (nilai === 2) {
+        console.log("dua")
+    } else {
+        console.log("angka lain");
+    }
+}
+
+jalansatu();

9bb0620c182361f3a88d6946a5502582ed425786
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-03 16:51:56 +0700

    add function example

---
+++ js-exmp/function.js
@@ -0,0 +1,20 @@
+function jalansatu() {
+    console.log("Jalan Satu");
+}
+
+const jalandua = function () {
+    console.log("Jalan Dua");
+}
+
+jalansatu();
+jalandua();
+
+// dua diatas menggunakan penulisan metode lama 
+
+const jalantiga = ()=> {
+    console.log("Jalan Tiga");
+}
+
+jalantiga();
+
+// yang terakhir menggunakan penulisan metode baru

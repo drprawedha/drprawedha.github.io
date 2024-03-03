6fd913a1da74eb907e9c7652fb69cc16fff23e94
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-03 16:35:39 +0700

    array test

---
+++ js-exmp/array.js
@@ -0,0 +1,2 @@
+var angka = [6,5,4,3,2,1];
+console.log(angka[0]);
--- js-exmp/index.js
+++ js-exmp/index.js
@@ -1,5 +1,12 @@
-var message = "hello coding";
-let msg1 = "hello1"
-const msg2 = "world"
+var nama1 = "Rhesa";
+let nama2 = "Prawedha";
+const age3 = "30";
+oth4 = "Selamat";
+// console.log(message, msg1, msg2);
 
-console.log(message, msg1, msg2);
+if (true) {
+}
+
+console.log(nama1);
+console.log(nama2);
+console.log(age3);

3a83e5df2353179a96b894ec26a518045a3beeae
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-16 15:06:19 +0700

    penambahan jalandua()

--- js-exmp/if-else.js
+++ js-exmp/if-else.js
@@ -1,12 +1,29 @@
 function jalansatu() {
-    var nilai = 1;
-    if (nilai === 3) {
-        console.log("tiga")
-    } else if (nilai === 2) {
-        console.log("dua")
+    var nilai = 2;
+    // if (nilai === 3) {
+    //     console.log("tiga")
+    // } else if (nilai === 2) {
+    //     console.log("dua")
+    // } else {
+    //     console.log("angka lain");
+    // }
+    if(nilai === 3)console.log("OK");
+    else console.log("bukan");
+}
+
+jalansatu(); 
+
+function jalandua() {
+    var nilai = 5;
+    if (nilai === 10) {
+        console.log("sepuluh");
+    } else if (nilai === 5) {
+        console.log("lima");
     } else {
         console.log("angka lain");
     }
 }
 
-jalansatu();
+jalandua()
+
+// ada cara lain

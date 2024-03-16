d758b5f14319e3d7a017615ee566b0f1aacf0231
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-16 15:21:35 +0700

    add jalandua()

--- js-exmp/if-elseif.js
+++ js-exmp/if-elseif.js
@@ -1,10 +1,10 @@
 var x = 2;
 
 if (x>20)
-console.log("x lebih besar dari 20");
+    console.log("x lebih besar dari 20");
 else if (x>10)
-console.log("x lebih besar dari 10");
+    console.log("x lebih besar dari 10");
 else if (x<5)
-console.log("x kurang dari 5");
+    console.log("x kurang dari 5");
 else
-console.log("x adalah 10");
+    console.log("x adalah 10");

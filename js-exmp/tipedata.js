3a3e3aeaf0c9f385ab8f784346ffb2d765cc0a26
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-17 16:05:20 +0700

    update sample typedata

---
+++ js-exmp/tipedata.js
@@ -0,0 +1,16 @@
+function tipedata() {
+    var data = 2;
+    console.log(typeof(data));
+
+    if(data === '2') {
+        return 'ok'
+    } else {
+        return 'gagal'
+    }
+    
+}
+// 
+console.log(tipedata());
+
+// === menandakan case sensitive
+// == menamdakal tolerir asal datanya StorageManager

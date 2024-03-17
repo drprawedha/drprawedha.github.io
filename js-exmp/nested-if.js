6794c6e776f42d013ba81d2573602b350b61c8fb
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-17 16:04:51 +0700

    update sample nested if

---
+++ js-exmp/nested-if.js
@@ -0,0 +1,19 @@
+var x = 31;
+var y = 20;
+var z = 30;
+
+
+if (x>y) {
+    if (x>z) {
+        console.log("X adalah yang terbesar");
+    } else {
+        console.log("X adalah lebih kecil dari Z, dan lebih besar dari Y");
+    }
+} else {
+    if (x<y) {
+        console.log("X adalah yang terkecil");
+    } else {
+        console.log("X lebih kecil dari pada Z, namun tidak pada Z");
+    }
+
+}

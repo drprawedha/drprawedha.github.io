6173967b2320f477adf316ad76819c4ca84ecf56
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-21 13:02:28 +0700

    update while.js

--- js-exmp/while.js
+++ js-exmp/while.js
@@ -4,3 +4,6 @@ while (i < 10) {
     console.log("Urutan ke ", i);
     i++
 }
+
+// pada while dia akan mengecheck kondisi terlebih dahulu.
+// sedangkan do melakukan eksekusi dari statement lalu check kondisi.

9357533a0e920bf5588fd9d107c2b344402a1904
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-03-21 13:38:03 +0700

    add infinity loop using for and while

---
+++ js-exmp/infinity.js
@@ -0,0 +1,14 @@
+// for(var i=0; i>=0;i++){
+//     console.log(i);
+// }
+
+// type 1 using for ==========
+
+
+var i = 1;
+while (true) {
+    console.log(i);
+    i++;
+}
+
+// type 2 using while

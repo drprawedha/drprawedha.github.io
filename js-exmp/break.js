9e95bc0903149d4a29a276042bc57e7ebb6d4a24
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-09 16:34:00 +0700

    update tugas 5.js dengan perulangan yang ditampilkan adalahy yang habis dibagi 3 [3,6,9,12,15,18]

--- js-exmp/break.js
+++ js-exmp/break.js
@@ -1,9 +1,10 @@
-for (var i=0; i<=10 ; i ++) {
-    if (i==5) {
-        continue;
+for (var i=0; i<=20 ; i ++) {
+    if (i % 3 === 0) {
+        // continue;
         //break; dan continue;
+        console.log(i);
     }
-    console.log(i);
+   
 
 }
 // pertama tampilkan repeat 0 sd 10

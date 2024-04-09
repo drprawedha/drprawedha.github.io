f220a2b722d0ff5b4c5c01fbc998b752da03f9fd
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 03:58:31 +0700

    add nested-object.js

---
+++ js-exmp/nested-object.js
@@ -0,0 +1,16 @@
+function NestedObject() {
+    var mahasiswa = {
+        nama: "rhesa",
+        
+        ipk: {
+            semester1 : 3.6,
+            semester2 : 3.7,
+            semester3 : 3.9
+        }
+            
+
+    }
+    console.log(mahasiswa.ipk.semester1);
+}
+
+NestedObject()

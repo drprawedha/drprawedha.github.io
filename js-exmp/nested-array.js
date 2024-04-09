dfd6c6459d0b39a811d0f450b4b28a5f797f482a
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 01:14:57 +0700

    add nested array

---
+++ js-exmp/nested-array.js
@@ -0,0 +1,14 @@
+function panggilNestedArray() {
+    var DataNAma = [["Ab","Ac","Ad"],
+                    ["Bola1","Bola2","Bola3"],
+                    ["Rem1","Rem2","Rem3"]]
+
+console.log(DataNAma.length);
+console.log(DataNAma[0][0]);
+console.log(DataNAma[0][1]);
+console.log(DataNAma[1][0]);
+console.log(DataNAma[2][2]);
+}
+
+
+panggilNestedArray()

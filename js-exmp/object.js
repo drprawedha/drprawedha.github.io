7b7ddf6e0e3290d9d840f882fc5118a12743f6b1
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 03:03:08 +0700

    update object.js

---
+++ js-exmp/object.js
@@ -0,0 +1,10 @@
+function AmbilObject() {
+    var mobil = {
+        type: "Sedan",
+        harga: 0,
+        warna: "merah"}
+    console.log(mobil);
+    console.log(mobil.type);
+}   
+
+AmbilObject();

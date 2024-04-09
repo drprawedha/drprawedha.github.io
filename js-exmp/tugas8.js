8e97fc7bcd4bf7f05478dc33452d9d8075acaaaa
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 03:26:29 +0700

    add Tugas 8

---
+++ js-exmp/tugas8.js
@@ -0,0 +1,14 @@
+function AmbilBio() {
+    var Bio = {
+        nama: "Rhesa Prawedha",
+        tempatlahir: "Sukoharjo",
+        tanggallahir: "20-04-1993",
+        domisili: "Sukoharjo",
+    }
+    Bio.pekerjaan = "Pengusaha"
+    console.log(Bio);
+    console.log(Bio.nama);
+    console.log(Bio.pekerjaan);
+}
+
+AmbilBio()

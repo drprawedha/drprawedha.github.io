dae42760aa45fc0429f2095fd5dd910fa4f01101
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 04:14:53 +0700

    add value

--- js-exmp/nested-object.js
+++ js-exmp/nested-object.js
@@ -5,12 +5,15 @@ function NestedObject() {
         ipk: {
             semester1 : 3.6,
             semester2 : 3.7,
-            semester3 : 3.9
+            semester3 : 3.9,
+            semester4 : 3.4,
+            semester4 : 3.5,
+            semester6 : 3.26,
         }
             
 
     }
-    console.log(mahasiswa.ipk.semester1); //pemanggilan utama, dari sini
+    console.log(mahasiswa.ipk.semester6); //pemanggilan utama, dari sini
 }
 
 NestedObject()

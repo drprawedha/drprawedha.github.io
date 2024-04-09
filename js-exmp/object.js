03c080af1c8a3042b7caac49e6b3fc2c4854c76a
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-10 03:04:26 +0700

    add array inside object

--- js-exmp/object.js
+++ js-exmp/object.js
@@ -2,9 +2,11 @@ function AmbilObject() {
     var mobil = {
         type: "Sedan",
         harga: 0,
-        warna: "merah"}
-    console.log(mobil);
-    console.log(mobil.type);
+        warna: "merah",
+        tahun: [2001,2004,2008,2012]
+    }
+    // console.log(mobil);
+    console.log(mobil.tahun[1]);
 }   
 
 AmbilObject();

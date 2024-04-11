e435d8f1c7025034f6545d85ba91e3024a7e2e16
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-11 12:04:03 +0700

    adding logika.js

---
+++ js-exmp/logika.js
@@ -0,0 +1,5 @@
+var x = 10;
+
+console.log(x>10 && x<20); // operand and, jika satu salah maka false
+console.log(x>10 || x<20); // operand or, jika satu benar maka true
+console.log(!true); // negasi 

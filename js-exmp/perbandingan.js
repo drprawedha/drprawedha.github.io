caa2961d665f68fcef752fa2d57caf38c4a80cf5
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-11 10:44:11 +0700

    adding perbandingan.js

---
+++ js-exmp/perbandingan.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 11;
+
+console.log(a>b); // lebih besar dari 
+console.log(a<b); // kurang dari 
+console.log(a>=b); // lebih dari sama dengan
+console.log(a<=b); // kurang dari sama dengan
+console.log(a==b); // sama dengan
+console.log(a===b); // sama dengan, type data harus sama
+console.log(a!=b); // tidak sama dengan
+console.log(a!==b); // tidak sama dengan, type data harus sama

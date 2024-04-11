71107eb5f8558d4b89c8afe10143b4db9eb3f2c1
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-11 13:25:17 +0700

    modified

--- js-exmp/tugas10.js
+++ js-exmp/tugas10.js
@@ -8,7 +8,7 @@ var angka_sebelumnya = 0;
 // Loop untuk menghasilkan urutan angka
 for (var i = 1; i <= N; i++) {
     // Tambahkan nilai (i - 1) ke angka_sebelumnya
-    angka_sebelumnya += i - 1;
+    angka_sebelumnya += i - 1 ;
 
     // Cetak angka_sebelumnya
     console.log(angka_sebelumnya);

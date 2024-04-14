cb525f3a6f4ccf4ea538b3ad9ba7c2edd2b03e34
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-14 09:48:22 +0700

    add comment for array splice

--- js-exmp/array-splice.js
+++ js-exmp/array-splice.js
@@ -3,7 +3,9 @@ function panggilSplice() {
     console.log(kota);
     // kota.splice(3,0,'solo')
     // kota.splice(3,1,'solo')
-    kota.splice(3,2,'solo')
+    // kota.splice(3,2,'solo')
+    kota.splice(2,4)            //untuk menghapus (nomor data , berapa data setelahnya yang dihapus)
+    // kota.splice(3,1)
     return kota;
 }
 

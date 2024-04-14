ae16d77ebcac19643b2f185963539aa198530509
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-14 09:28:35 +0700

    add tugas 12.js

---
+++ js-exmp/tugas12.js
@@ -0,0 +1,11 @@
+function tugasshiftpop() {
+    var buah = ['pisang','jeruk','apel','mangga'];
+    console.log(buah);
+    buah1=buah.pop()
+    //console.log(buah1);
+    console.log(buah);
+    buah2=buah.shift()
+    console.log(buah);
+}
+
+tugasshiftpop()

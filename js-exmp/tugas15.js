a3f9e2c6c07046e9f46372949d3aab05e3e9760a
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-15 21:58:38 +0700

    add sample pow and sqrt at phytagorean triples

---
+++ js-exmp/tugas15.js
@@ -0,0 +1,17 @@
+var ab = 3
+var bc = 4
+
+function luassegitiga() {
+    console.log("nilai ab adalah",ab);
+    console.log("nilai bc adalah",bc);
+    abp2 = Math.pow(ab,2)
+    // console.log(abp2);
+    bcp2 = Math.pow(bc,2)
+    // console.log(bcp2);
+    acp2 = (abp2+bcp2)
+    // console.log(acp2);
+    ac = Math.sqrt(acp2)
+    console.log("panjang sisi ac adalah",ac);
+}
+
+luassegitiga()

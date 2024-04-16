4add066b17262af81e751f2779881eeb6582f2bb
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-16 18:36:50 +0700

    adding sample code at . js

---
+++ js-exmp/codeat.js
@@ -0,0 +1,7 @@
+// digunakan untuk menampilkan kode asli dalam sebuah karakter
+function CodeAt() {
+    var kode = ".hello world!" //cari tanda titik
+    console.log(kode.charCodeAt(2));
+}
+
+CodeAt()

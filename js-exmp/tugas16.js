f149e899817ef452b1e2f0ee996eeffe17355019
Rhesa Prawedha <dr.prawedha@gmail.com>
2024-04-16 18:28:26 +0700

    adding sample min max at tugas16.js

---
+++ js-exmp/tugas16.js
@@ -0,0 +1,9 @@
+function minmax() {
+    minn = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    maxn = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    console.log("nilai min :",minn);
+    console.log("nilai max :",maxn);
+    
+}
+
+minmax()
